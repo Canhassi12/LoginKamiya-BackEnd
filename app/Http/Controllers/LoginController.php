5c2fcf534af5e0688a9a324fe2e26fc378bd24da
Canhassi <arthurcanhassi@gmail.com>
2022-07-24 18:42:54 -0300

    feat: register, validation and save data

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use Illuminate\Support\Facades\Hash;
+
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:55|',
+            'email' => 'required|unique:users',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            $messages = $validator->getMessageBag()->first();
+
+            return response()->json($messages, 403);
+        }
+
+        $inputs = $request->all();
+
+        $name = $inputs['name'];
+        $email = $inputs['email'];
+        $password = Hash::make($inputs['password']);
+
+        try {
+            $user = [
+                'name' => $name,
+                'email' => $email,
+                'password' => $password, 
+            ];
+
+            User::create($user); 
+        
+        } catch (Exception $e) {
+            return response()->json($e, 500);
+        }
+        
+        return response()->json('User has been added in database with sucessful', 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // Arthur Roque Nogueira Canhassi romulo dias
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
