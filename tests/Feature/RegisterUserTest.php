0835f0e0d4a5603b06c97a4b5126d484180c7730
Canhassi <arthurcanhassi@gmail.com>
2022-07-24 18:43:37 -0300

    feat: Feature test, to register a user

---
+++ tests/Feature/RegisterUserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Validator;
+use App\Services\ValidationUser;
+
+class RegisterUserTest extends TestCase
+{
+     /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function if_user_data_is_added_in_database()
+    {
+        $newUser = User::factory()->make();
+
+        $newUser->name;
+        $newUser->email;
+        $newUser->password;
+
+        $token = csrf_token();
+
+        $this->post('/api/register', ['name' => $newUser->name, 'email' => $newUser->email, 'password' => $newUser->password, '_token' => $token])
+        ->assertSuccessful();
+    }
+}
