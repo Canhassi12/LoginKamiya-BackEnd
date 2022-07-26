d7d54b19c076cfc6f7faf7ab71b37b191b8a6195
Canhassi <arthurcanhassi@gmail.com>
2022-07-26 19:52:02 -0300

    feat: implement login tests

---
+++ tests/Feature/LoginUserTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginUserTest extends TestCase
+{
+    /**
+     *
+     * @test
+     */
+    public function user_login_and_return_access_token(): void
+    {
+        $user = User::factory()->create();
+
+        $token = csrf_token();
+
+        $response = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => 'linguica2',
+            '_token' => $token
+        ])
+        ->assertOk()
+        ->assertSee('token');
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function invalid_user_login(): void
+    {    
+        $this->post('/api/login', [
+            'email' => 'kamiya',
+            'password' => 'kamiya',
+        ])
+        ->assertStatus(401);   
+    }
+}
