7131291ae1e61a83c1e89601cb2f63fde763bff6
Canhassi <arthurcanhassi@gmail.com>
2022-07-26 19:49:55 -0300

    feat: login route

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [UserController::class, 'store']);
+Route::post('/login', [UserController::class, 'login']);
+
+Route::post('/register', [UserController::class, 'register']);
